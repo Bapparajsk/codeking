c7dd1e9021258bdfe9e99a0ed7c8e1fae1cbf70b
bapparaj <bapparaj97@gmail.com>
2024-01-30 18:08:58 +0530

    subtitle boxes styling complete

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -2,11 +2,11 @@ import '$/footer/style.css'
 
 export const Footer = () => {
     return (
-        <>
+        <div>
             <hr className={'hr'}/>
             <footer className={'footer'}>
                 <p>Copyright © 2023 Code King</p>
             </footer>
-        </>
+        </div>
     )
 }
--- src/style/problem/SubBox.jsx
+++
@@ -1,7 +0,0 @@
-export const SubBox = props => {
-    return (
-        <div className={"p-sub-card"}>
-
-        </div>
-    )
-}
