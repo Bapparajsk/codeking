8a6b986003f26c90a9c045d96a915976ac6950b5
bapparaj <bapparaj97@gmail.com>
2024-02-05 19:53:50 +0530

    problem Editor Setting fixes

--- src/component/problem/showProblem/EditorBox.jsx
+++ src/component/problem/showProblem/EditorBox.jsx
@@ -1,28 +1,68 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import "$/problem/showProblem/editorBox.css"
 import { Editor } from '@monaco-editor/react';
 import {Languages} from "#/problem/showProblem/Ianguages";
 
 export const EditorBox = () => {
 
-    const handleClick = () => {
+    const [lan_box, setLan_box] = useState(false);
+    const [v, setV] = useState(1.0);
+    const [currLAn, setCurrLAn] = useState("java");
 
+    const iconRef = useRef();
+
+    const removeLanBox = () => {
+        let intervalId = setInterval(() => {
+            setV((prevV) => {
+                const newValue = prevV - 0.1;
+
+                if (newValue <= 0.0) {
+                    clearInterval(intervalId);
+                    iconRef.current.style.transform = `rotate(0deg)`;
+                    setLan_box(false);
+                }
+
+                return newValue;
+            });
+        }, 100);
     }
+
+    const setLan = (e) => {
+        setCurrLAn(e)
+        removeLanBox();
+    }
+
+    const handleClick = () => {
+        const rotateDegree = lan_box ? 0 : 180;
+
+
+        if (lan_box) {
+            removeLanBox();
+        } else {
+            iconRef.current.style.transform = `rotate(180deg)`;
+            setV(1.0);
+            setLan_box(true);
+        }
+    }
+
+    useEffect(() => {
+        iconRef.current.style.transition = "all .4s ease-out";
+    }, []);
 
     return (
         <div className={'editor-box flex'}>
             <div className={"editor-nav flex"}>
                 <div className={"editor-lan-box flex"}>
                     <div className={"lan-box flex"} onClick={handleClick}>
-                        <span>java</span>
-                        <i className="fa-solid fa-angle-down"></i>
+                        <span>{currLAn}</span>
+                        <i ref={iconRef} className="fa-solid fa-angle-down"></i>
                     </div>
-                    <Languages/>
+                    {lan_box && <Languages opa={v} setLan={setLan} currLAn={currLAn}/>}
                 </div>
                 <div className={"editor-run-box flex"}>
-                <div className={"run-box"}>
+                <div className={"run-box flex"}>
                         <span>Run</span>
-                        <i className="fa-solid fa-play"></i>
+                        <i className="fa-solid fa-play fa-xs"></i>
                     </div>
                     <div className={"submit-box"}>
                         <span>Submit</span>
@@ -41,7 +81,6 @@ export const EditorBox = () => {
                     language={'java'}
                     theme={"light"}
                     defaultValue={"class codeKing {\n\tpublic int solve(int n) {\n\n\t}\n}"}
-
                 />
             </div>
         </div>
--- src/component/problem/showProblem/Ianguages.jsx
+++ src/component/problem/showProblem/Ianguages.jsx
@@ -1,7 +1,26 @@
-export const Languages = () => {
-    return (
-        <div style={{width: 200, height: 100, backgroundColor: "green", position: "absolute", top: "120px", left: 0, zIndex: 99}}>
+export const Languages = ({ opa, setLan, currLAn }) => {
 
+    const lan = [ "java", "c", "cpp", "python", "javaScript" ];
+
+    const handleClick = (selectedLanguage) => {
+        if (setLan) {
+            setLan(selectedLanguage);
+        }
+    };
+
+    return (
+        <div className={"editor-languages-box flex"} style={{opacity: opa}}>
+            {
+                lan.map((value, index) => (
+                    <span
+                        key={index}
+                        className={`flex ${currLAn === value ? 'is-current' : ''}`}
+                        onClick={() => handleClick(value)}
+                    >
+                        {value}
+                    </span>
+                ))
+            }
         </div>
     )
 }
