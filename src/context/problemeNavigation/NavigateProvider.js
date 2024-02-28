6fef79c699a8dc4a05605545ab35703f0b3e2aae
bapparaj <bapparaj97@gmail.com>
2024-02-28 19:44:18 +0530

    add problem context

---
+++ src/context/problemeNavigation/NavigateProvider.js
@@ -0,0 +1,19 @@
+// context/YourContextProvider.js
+"use client"
+import { useState, createContext, useContext } from 'react';
+import { } from 'next'
+
+const Navigate = createContext();
+
+const NavigateProvider = ({ children }) => {
+
+    return (
+        <Navigate.Provider value={{yourState}}>
+            {children}
+        </Navigate.Provider>
+    );
+};
+
+const useNavigateRouter = useContext(Navigate);
+
+export { NavigateProvider, useNavigateRouter };
